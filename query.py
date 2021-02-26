b2925f4dcc0a8bd9b78c6e54eb797b7f912a8258
Tom Smeitink <tsmeitink@matrixiangroup.com>
2021-02-26 09:18:34 +0100

    added group by query example

--- query.py
+++ query.py
@@ -1,7 +1,15 @@
+from sqlalchemy import func
 from objects import Actor, Film
 from crud import session_scope
 
+# Regular join query
 with session_scope() as s:
-    result = s.query(Film, Actor).join(Actor, Film.actors).filter(Film.title == "Fight Club")
-    for film, actor in result:
+    results = s.query(Film, Actor).join(Actor, Film.actors).filter(Film.title == "Fight Club")
+    for film, actor in results:
         print(film.title, actor.name)
+
+# Group by query
+with session_scope() as s:
+    result = s.query(Film.title, func.count(Actor.name)).join(Actor, Film.actors).group_by(Film.title)
+    print(result)
+    print(result.all())
