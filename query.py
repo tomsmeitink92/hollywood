de251833361dd71b697ba0c9feee81cbf479e9c7
Tom Smeitink <tsmeitink@matrixiangroup.com>
2021-02-26 16:28:35 +0100

    added queries

--- query.py
+++ query.py
@@ -1,5 +1,5 @@
-from sqlalchemy import func
-from models import Actor, Film
+from sqlalchemy import func, text
+from models import Actor, Film, Director
 from crud import session_scope
 
 # Regular join query
@@ -8,8 +8,29 @@ with session_scope() as s:
     for film, actor in results:
         print(film.title, actor.name)
 
-# Group by query
+# Group by query: Group film with the largest cast
 with session_scope() as s:
-    result = s.query(Film.title, func.count(Actor.name)).join(Actor, Film.actors).group_by(Film.title)
-    print(result)
-    print(result.all())
+    result = (
+        s.query(Film.title, func.count(Actor.name).label("count"))
+        .join(Actor, Film.actors)
+        .group_by(Film)
+        .order_by(text("count DESC"))
+        .limit(1)
+        .all()
+    )
+    for title, count in result:
+        print(title, count)
+
+# Group by query: Group actor that has worked with the most directors
+with session_scope() as s:
+    result = (
+        s.query(Actor.name, func.count(Director.name).label("count"))
+        .join(Film, Actor.films)
+        .join(Director, Film.directors)
+        .group_by(Actor)
+        .order_by(text("count DESC"))
+        .limit(1)
+        .all()
+    )
+    for actor, count in result:
+        print(actor, count)
