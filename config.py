00b936e6518f257fbc88ed90fb56535268fc6294
Tom Smeitink <tsmeitink@matrixiangroup.com>
2021-02-26 08:42:02 +0100

    added documentation to crud.py and inserts.py

--- config.py
+++ config.py
@@ -11,7 +11,14 @@ usr = getenv("user")
 pwd = getenv("password")
 db = getenv("database")
 
+# Define the connection string used to connect to our PostgreSQL database
 db_string = f"postgres://{usr}:{pwd}@{host}/{db}"
+
+# Create the search engine for SQLAlchemy
 engine = create_engine(db_string)
+
+# Add a declarative base class, to be inherited by our models/objects
 Base = declarative_base()
+
+# Define a session, which "manages persistence operations for ORM-mapped objects"
 session = Session(engine)
